5dac1caedd301037db6b2be82c8fb96588a936e9
Kylee W <23221736+raspberrymilkyway@users.noreply.github.com>
2023-05-02 17:03:39 -0500

    footer
    migrated header stuff too

--- WebDev2Final/FinalProject/src/app/app.module.ts
+++ WebDev2Final/FinalProject/src/app/app.module.ts
@@ -5,17 +5,19 @@ import { AppComponent } from './app.component';
 import { QuizComponent } from './quiz/quiz/quiz.component';
 import { GradeComponent } from './quiz/grade/grade.component';
 import { FormsModule } from '@angular/forms';
-import { HeaderComponent } from './header/header/header.component';
+import { HeaderComponent } from './header/header.component';
 
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuizComponent,
     GradeComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
