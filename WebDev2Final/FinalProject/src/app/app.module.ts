b2f861ba146376a19ae38a867008060d50d91e68
Kylee W <23221736+raspberrymilkyway@users.noreply.github.com>
2023-05-03 17:25:48 -0500

    added header stuff
    no real formatting, will fix later

--- WebDev2Final/FinalProject/src/app/app.module.ts
+++ WebDev2Final/FinalProject/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { FooterComponent } from './footer/footer.component';
 
 // import { RouterModule } from '@angular/router';
 import { RoutingModule } from './routing.module';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -30,7 +31,8 @@ import { RoutingModule } from './routing.module';
     MatToolbarModule,
     BrowserAnimationsModule,
     // RouterModule,
-    RoutingModule
+    RoutingModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
